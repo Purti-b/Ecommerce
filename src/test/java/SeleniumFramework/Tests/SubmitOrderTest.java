5bedffe3d38013a4a91b679ed16451212265207f
purti <purtibhalerao97@gmail.com>
2025-07-20 15:27:03 +0530

    data provider

--- src/test/java/SeleniumFramework/Tests/SubmitOrderTest.java
+++ src/test/java/SeleniumFramework/Tests/SubmitOrderTest.java
@@ -10,17 +10,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class SubmitOrderTest extends BaseTest {
-    String productName = "ZARA COAT 3";
+    //String productName = "ZARA COAT 3";
 
-    @Test
+    @Test(dataProvider = "getData",groups = "Purchase")
 
-    public void submitOrder() throws InterruptedException, IOException {
+    public void submitOrder(String email, String password, String productName) throws InterruptedException, IOException {
         //WebDriverManager.chromedriver().setup();
 
-        ProductCatalogue productCatalogue = landingPage.loginApplication("purti@gmail.com","Purti@2278");
+        ProductCatalogue productCatalogue = landingPage.loginApplication(email,password);
 
         List<WebElement> products = productCatalogue.getProductList();
         productCatalogue.addProductToCart(productName);
@@ -38,13 +39,18 @@ public class SubmitOrderTest extends BaseTest {
 
     }
 
-    @Test(dependsOnMethods = "submitOrder")
-    public void viewOrder() throws IOException, InterruptedException {
-        ProductCatalogue productCatalogue = landingPage.loginApplication("purti@gmail.com","Purti@2278");
+    @Test(dependsOnMethods = "submitOrder",dataProvider = "getData")
+    public void viewOrder(String email, String password, String productName) throws IOException, InterruptedException {
+        ProductCatalogue productCatalogue = landingPage.loginApplication(email,password);
         OrderPage orderPage = productCatalogue.goToOrder();
 
         Boolean match = orderPage.verifyOrderDisplay(productName);
         Assert.assertTrue(match);
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+        return new Object[][] {{"purti@gmail.com","Purti@2278","ZARA COAT 3"},{"anshika@gmail.com","Iamking@000","ADIDAS ORIGINAL"}};
     }
 
 }
